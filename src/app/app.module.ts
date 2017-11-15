c2527f23ca26c50ab7fefc3f502722bb972256e0
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2017-11-15 18:08:10 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { TouchID } from '@ionic-native/touch-id';
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +24,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TouchID
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TouchID } from '@ionic-native/touch-id';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+  	public touchId: TouchID) {
 
   }
+openTouchId(){
+this.touchId.isAvailable()
+  .then(
+    res => {
+    	this.touchId.verifyFingerprint('Scan your fingerprint please')
+  .then(
+    res => console.log('Ok', res),
+    err => console.error('Error', err)
+  );
+    },
+    err => console.error('TouchID is not available', err)
+  );
 
+
+}
 }
